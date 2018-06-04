91a686089c82792fd67c1d8c0fe22fb32d375c5b
Yihao Yang <yihaoyan@usc.edu>
2018-06-04 01:11:04 -0700

    Spawning Projectile

---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 10.0f;
+    public float padding = 0.5f;
+    public GameObject projectile;
+    public float projectileSpeed = 3.0f;
+    public float firingRate = 0.2f;
+
+    float xmin;
+    float xmax;
+
+    private void Start()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+    }
+    void Fire(){
+        GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            InvokeRepeating("Fire", 0.0000001f, firingRate);
+        }
+        if (Input.GetKeyUp(KeyCode.Space)){
+            CancelInvoke("Fire");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)){
+           transform.position += Vector3.left * speed * Time.deltaTime; //transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime; //transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
