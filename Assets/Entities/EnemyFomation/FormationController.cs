e7102ed149ca24c40912c7bb9d8037bcdb39b1d8
Yihao Yang <yihaoyan@usc.edu>
2018-06-04 02:13:36 -0700

    Detecting enemies have been destroyed

---
+++ Assets/Entities/EnemyFomation/FormationController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormationController : MonoBehaviour {
+    public GameObject enemyPrefab;
+    public float width = 8f;
+    public float height = 4f;
+    private bool movingRight = true;
+    public float speed = 3f;
+
+    private float xmax;
+    private float xmin;
+
+    // Use this for initialization
+    void Start()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmax = rightmost.x;
+        xmin = leftmost.x;
+
+        Respawn();
+    }
+
+    void Respawn(){
+        foreach (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity);
+            enemy.transform.parent = child;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingRight)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        float rightEdgeOfFormation = transform.position.x + (0.5f * width);
+        float leftEdgeOfFormation = transform.position.x - (0.5f * width);
+        if (leftEdgeOfFormation < xmin)
+        {
+            movingRight = true;
+        }
+        else if (rightEdgeOfFormation > xmax)
+        {
+            movingRight = false;
+        }
+
+        if (AllMembersDead()){
+            Respawn();
+        }
+    }
+
+    bool AllMembersDead(){
+        foreach (Transform childPositionGameObject in transform){
+            if (childPositionGameObject.childCount > 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
