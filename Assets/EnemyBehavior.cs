721648ae367d9316d0a7f936017d71fe6e96a4ca
Yihao Yang <yihaoyan@usc.edu>
2018-06-04 01:29:28 -0700

    Getting Damage from Laser

---
+++ Assets/EnemyBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col){
+        Projectile missile = col.gameObject.GetComponent<Projectile>();
+        if (missile){
+            
+        }
+    }
+}
