26c280085a7244bb067869f987c6f3642fdb01d0
Yihao Yang <yihaoyan@usc.edu>
2018-06-04 00:25:35 -0700

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 10.0f;
+    public float padding = 0.5f;
+
+    float xmin;
+    float xmax;
+
+    private void Start()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow)){
+           transform.position += Vector3.left * speed * Time.deltaTime; //transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime; //transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
